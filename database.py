85d68add913007758a2ed2bc79b2ece6c26fb8b4
KyleN29 <gcarey0@outlook.com>
2024-12-03 02:40:48 -0600

    database file test

---
+++ database.py
@@ -0,0 +1,12 @@
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+
+# MongoDB Connection
+uri = os.getenv("uri_key")
+client = MongoClient(uri, server_api=ServerApi('1'))
+
+# Initialize MongoDB Collections
+db = client["valorant"]
+users = db["users"]
+mmr_collection = db["mmr_data"]
+all_matches = db["matches"]
